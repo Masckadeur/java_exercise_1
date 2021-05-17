6d9ca6739d2c86a18f8e2ea9a37a8d18cc7e7d55
erwan benard <erwan.benard@epita.fr>
2021-05-17 22:40:08 +0200

    Part 4 Launcher add fibo command

--- src/Launcher.java
+++ src/Launcher.java
@@ -12,7 +12,28 @@ public class Launcher {
             if (cmd.equals("quit")) {
                 break;
             }
+            else if (cmd.equals("fibo")) {
+                System.out.println("Entrez la valeur n désirée de la suite de fibonacci");
+                cmd = scan.nextLine();
+                int n = Integer.parseInt(cmd);
+                if (n == 0 || n == 1) {
+                    System.out.println("fibo(" + cmd + ") = 1");
+                }
+                else {
+                    int a = 1;
+                    int b = 1;
+                    Integer c = 1;
+                    for (int i = 2; i < n; i++) {
+                        c = a + b;
+                        a = b;
+                        b = c;
+                    }
+                    System.out.println("fibo(" + cmd + ") = " + c.toString());
+                }
+            }
+            else {
             System.out.println("Unknown command");
+            }
         }
     }
 }
